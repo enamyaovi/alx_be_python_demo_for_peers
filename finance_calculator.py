119852a21546c68e37ab8948ab0207ee9f49a896
Cephas Yaovi Tay <cephas.tay137@gmail.com>
2024-09-15 20:11:29 +0000

    Update finance_calculator.py
    just copied and pasted the original code

--- finance_calculator.py
+++ finance_calculator.py
@@ -1,4 +1,15 @@
-# This is my Finance Calculator script. 
+#This file helps users calculate their monthly savings
 
-#Defining the variables in the script
-monthly_income = float(input('Enter your monthly income:'))
+#defining variables from user inputs
+monthly_income = int(input('Enter your monthly income: '))
+monthly_expenses = int(input('Enter your monthly expenses: '))
+
+#new variables
+monthly_savings = monthly_income - monthly_expenses
+rate = 0.05
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05) #calculates the projected savings at 5% interest rate
+
+r_projected_save = int(projected_savings)
+
+print('Your monthly savings are $' +str(monthly_savings)+ '.')
+print('Projected savings after one year, with interest, is: $'+str(r_projected_save)+'.')
